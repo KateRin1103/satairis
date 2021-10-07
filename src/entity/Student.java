66dcb1737cdf86474fc81a726552bd740391416a
Kristina <isakovakristina666@gmail.com>
2021-10-07 15:40:23 +0300

    edit Student.java

--- src/entity/Student.java
+++ src/entity/Student.java
@@ -4,4 +4,28 @@ public class Student {
     int age;
     String name;
     String eyes;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEyes() {
+        return eyes;
+    }
+
+    public void setEyes(String eyes) {
+        this.eyes = eyes;
+    }
 }
